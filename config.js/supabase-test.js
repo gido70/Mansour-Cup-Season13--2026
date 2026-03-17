5a9f38fb7bf50574fdab323ea1f581bd294243a6
gido70 <63196885+gido70@users.noreply.github.com>
2026-03-17 19:40:20 +0400

    Add files via upload

---
+++ config.js/supabase-test.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://npoowwdjpbvhyjrmeqkv.supabase.co";
+const SUPABASE_KEY = "sb_publishable_VVHZURoVGvIV8Ys-lQOL5w_nR-Ayhw8";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
